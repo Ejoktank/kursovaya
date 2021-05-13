fda45f1fa3016fe1974daf46b17b0eb6f0de96f5
egor_kalmykov <egor.v.kalmykov@gmail.com>
2021-05-13 15:18:16 +0300

    initial commit

---
+++ Хищникжертва.cpp
@@ -0,0 +1,87 @@
+
+#include "stdafx.h"
+double f1(double*);
+double f2(double*);
+double f3(double*);
+double f4(double*);
+void eiler(double, double*, double*, double, double(*f[4])(double*));
+double(*f[4])(double*);
+
+int main()
+{
+	setlocale(LC_CTYPE, "russian");
+	ofstream ff("outt.txt");
+	ofstream ffa("outy0.txt");
+	ofstream ffb("outy1.txt");
+	ofstream ffc("outy2.txt");
+	ofstream ffd("outy3.txt");
+	double y0[4], y[4], t = 0, h;
+	int k = 0;
+	h = 0.01;
+	y0[0] = 0;
+	y0[1] = 0;
+	y0[2] = 0;
+	y0[3] = 1;
+	f[0] = f1;
+	f[1] = f2;
+	f[2] = f3;
+	f[3] = f4;
+	cout << "\nВторая модификация\n";
+	do
+	{
+		eiler(t, y0, y, h, f);
+		t += h;
+		k++;
+		if (k % 10 == 0)
+		{
+			cout << t << '\n';
+			cout.width(5);
+			cout << int(1000 * y[0]) / 1000. << ' ';
+			cout.width(5);
+			cout << int(1000 * y[1]) / 1000. << '\n';
+			ff << t << '\n';
+			ffa.width(5);
+			ffa << int(1000 * y[0]) / 1000. << '\n';
+			ffb.width(5);
+			ffb << int(1000 * y[1]) / 1000. << '\n';
+		}
+		y0[0] = y[0];
+		y0[1] = y[1];
+		y0[2] = y[2];
+		y0[3] = y[3];
+	} while (t < 5);
+	return 0;
+}
+double f1(double *y)
+{
+	return 50 * y[0] * (1 - y[0] / 4 - y[0] * y[1] / (1 + y[0] * y[0]));
+}
+double f2(double *y)
+{
+	return y[1] * (y[0] * y[0] / (1 + y[0] * y[0]) - 0.9);
+}
+double f3(double* y)
+{
+	return y[2] * (y[0] * y[0] / (1 + y[0] * y[0]) - 0.9);
+}
+double f4(double* y)
+{
+	return y[3] * (y[0] * y[0] / (1 + y[0] * y[0]) - 0.9);
+}
+
+void eiler(double x, double *y0, double *y, double h, double(*f[4])(double*))
+{
+	double *tmp;
+	tmp = new double[4];
+	tmp[0] = y0[0] + (*f[0])(y0) * h;
+	tmp[1] = y0[1] + (*f[1])(y0) * h;
+	tmp[2] = y0[2] + (*f[2])(y0) * h;
+	tmp[3] = y0[3] + (*f[3])(y0) * h;
+	y[0] = y0[0] + ((*f[0])(y0) + (*f[0])(tmp)) * h / 2;
+	y[1] = y0[1] + ((*f[1])(y0) + (*f[1])(tmp)) * h / 2;
+	y[2] = y0[2] + ((*f[2])(y0) + (*f[2])(tmp)) * h / 2;
+	y[3] = y0[3] + ((*f[3])(y0) + (*f[3])(tmp)) * h / 2;
+	delete tmp;
+}
+
+
